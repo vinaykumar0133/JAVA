b7acf79c4377c5186f7d660a0b75bca208b4da3e
vinaykumar0133 <201952210@iiitvadodara.ac.in>
2024-08-19 00:08:06 +0530

    Day 1 : JAVA
    Java Basics

---
+++ AddNumbers.java
@@ -0,0 +1,14 @@
+/**
+ * AddNumbers
+ */
+import java.util.*;
+public class AddNumbers {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        System.out.printf("sum of two numbers is %d",a+b);
+
+    }
+}

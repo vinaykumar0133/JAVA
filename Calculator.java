3a4f5901d3a2ce365225236dc59fca0c85586199
vinaykumar0133 <201952210@iiitvadodara.ac.in>
2024-08-19 00:12:17 +0530

    Day 1 : JAVA
    Basics of JAVA

---
+++ Calculator.java
@@ -0,0 +1,30 @@
+import java.util.*;;
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the first number: ");
+        int num1=scanner.nextInt();
+        System.out.println("Enter the second number: ");
+        int num2=scanner.nextInt();
+        System.out.println("Enter the operator (+, -, *, /): ");
+        String operator = scanner.next();
+        switch (operator) {
+            case "+":
+            System.out.println("Result: " + (num1 + num2));
+            break;
+            case "-":
+            System.out.println("Result: " + (num1 - num2));
+            break;
+            case "*" :
+            System.out.println("Result: " + (num1 * num2));
+            break;
+            case "/":
+            System.out.println("Result: " + (num1/num2));
+            break;
+            default:
+            System.out.println("Incorrect Choice");
+
+        }
+
+    }
+}

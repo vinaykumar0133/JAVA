c38119554f8dced8d6da6b8fed2a07047a7ecfbf
vinaykumar0133 <201952210@iiitvadodara.ac.in>
2024-08-21 00:41:48 +0530

    DAY2 : JAVA
    JAVA Patterns

---
+++ Loops.java
@@ -0,0 +1,27 @@
+public class Loops {
+    public static void main(String[] args) {
+        //for loop : Print first 10 even numbers
+        // for (int i = 0; i <=20; i++) {
+        //     if(i%2==0){
+        //         System.out.println(i);
+        //     }
+        // }
+
+        //while loop : Print first 10 odd numbers
+        // int i=0;
+        // while (i<=20) {
+        //     if(i%2==0){
+        //         System.out.println(i);
+        //     }
+        //     i++;
+        // }
+
+        //Do while loop
+        int i=0;
+        do{
+            if(i%2==0){
+            System.out.println(i);}
+            i++;
+        }while(i<=20);
+    }
+}
---
+++ Patterns.java
@@ -0,0 +1,141 @@
+import java.util.Scanner;
+
+public class Patterns {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        //solid rectangle
+        // for (int i = 0; i < 5; i++) {
+        //     for (int j = 0; j <=4; j++) {
+        //         System.out.print("*"+" ");
+        //     }
+        //     System.out.println("");
+        // }
+        // System.out.println("Enter First Number");
+        // int r=sc.nextInt();
+        // System.out.println("Enter First Number");
+        // int c=sc.nextInt();
+        //Hollow Rectangle
+        // for(int i=1;i<=rows;i++){
+        //     for(int j=1;j<=cols;j++){
+        //        if(i==1||j==1||i==rows||j==cols){
+        //         System.out.print("*"+" ");
+        //        } 
+        //        else{
+        //         System.out.print("  ");
+        //        }
+        //     }
+        //     System.out.println();
+        // }
+
+        //Half pyramid
+        // for(int i=1;i<=r;i++){
+        //     for(int j=1;j<=c;j++){
+        //         if(j<=i){
+        //             System.out.print("*"+" ");
+        //         }
+        //         else{
+        //             System.out.print("  ");
+        //         }
+        //     }
+        //     System.out.println();
+        // }
+
+        //Inverted Half pyramid
+        int temp=1;
+        int n=sc.nextInt();
+        // for(int i=1;i<=n;i++){
+        //     for(int j=1;j<=i;j++){
+        //          System.out.print(temp+"  ");
+        //          temp++;
+        //     }
+        //     System.out.println("  ");
+        // }
+
+        // for(int i=1;i<=n;i++){
+        //     for(int j=1;j<=i;j++){
+        //          if(i%2==0 && j%2==0 || i%2!=0 && j%2!=0){
+        //             System.out.print(1+" ");
+        //          }
+        //          else{
+        //             System.out.print(0+" ");
+        //          }
+        //     }
+        //     System.out.println();
+        // }
+
+        int count=1;
+
+        // for(int i=1;i<=n;i++){
+        //     if(i<n/2){
+        //          for(int k=1;k<=i;k++){
+        //             System.out.print("*");
+        //          }
+        //          for(int k=1;k<=n-2*i;k++){
+        //             System.out.print(" ");
+        //          }
+        //          for(int k=1;k<=i;k++){
+        //             System.out.print("*");
+        //          }
+
+        //        System.out.println("  ");
+        //         }
+        //         else if(n/2<i-1){
+        //             for(int k=1;k<=n-i+1;k++){
+        //                 System.out.print("*");
+        //              }
+        //              for(int k=1;k<=2*count;k++){
+        //                 System.out.print(" ");
+        //              }
+        //              for(int k=1;k<=n-i+1;k++){
+        //                 System.out.print("*");
+        //              }
+        //              count++;
+        //            System.out.println("  ");
+        //         }
+        //         else{
+        //             for(int k=1;k<=n;k++){
+        //                 System.out.print("*");
+        //             }
+        //             System.out.println();
+        //         }
+                
+        // }
+
+        for(int i=1;i<=n;i++){
+            if(i<n/2){
+                 for(int k=1;k<=i;k++){
+                    System.out.print("*");
+                 }
+                 for(int k=1;k<=n-2*i;k++){
+                    System.out.print(" ");
+                 }
+                 for(int k=1;k<=i;k++){
+                    System.out.print("*");
+                 }
+
+               System.out.println("  ");
+                }
+                else if(n/2<i){
+                    for(int k=1;k<=n-i+1;k++){
+                        System.out.print("*");
+                     }
+                     for(int k=1;k<=2*count;k++){
+                        System.out.print(" ");
+                     }
+                     for(int k=1;k<=n-i+1;k++){
+                        System.out.print("*");
+                     }
+                     count++;
+                   System.out.println("  ");
+                }
+                else{
+                    for(int k=1;k<=n;k++){
+                        System.out.print("*");
+                    }
+                    System.out.println();
+                }
+                
+        }
+        
+    }
+}

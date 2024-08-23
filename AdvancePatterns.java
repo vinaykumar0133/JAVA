9020fb695a12dc41aab68b62a1090ef26f3b7da7
vinaykumar0133 <201952210@iiitvadodara.ac.in>
2024-08-24 01:10:46 +0530

    JAVA : DAY 5
    Functions In java

--- AdvancePatterns.java
+++ AdvancePatterns.java
@@ -5,31 +5,92 @@ public class AdvancePatterns {
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
 
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=i;j++){
-                System.out.print("* ");
-            }
-            for(int j=1;j<=2*(n-i);j++){
-                System.out.print("  ");
-            }
-            for(int j=1;j<=i;j++){
-                System.out.print("* ");
-            }
+        // for(int i=1;i<=n;i++){
+        //     for(int j=1;j<=i;j++){
+        //         System.out.print("* ");
+        //     }
+        //     for(int j=1;j<=2*(n-i);j++){
+        //         System.out.print("  ");
+        //     }
+        //     for(int j=1;j<=i;j++){
+        //         System.out.print("* ");
+        //     }
 
+        //     System.out.println();
+        // }
+
+        // for(int i=n;i>=1;i--){
+        //     for(int j=1;j<=i;j++){
+        //         System.out.print("* ");
+        //     }
+        //     for(int j=1;j<=2*(n-i);j++){
+        //         System.out.print("  ");
+        //     }
+        //     for(int j=1;j<=i;j++){
+        //         System.out.print("* ");
+        //     }
+        //     System.out.println();
+        // }
+
+        // for(int i=1;i<=n;i++){
+        //     for(int j=1;j<=n-i;j++){
+        //         System.out.print(" ");
+        //     }
+        //     for(int j=1;j<=n;j++){
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        // }
+
+        // for(int i=1;i<=n;i++){
+
+        //     for(int j=1;j<=n-i;j++){
+        //         System.out.print(" ");
+        //     }
+
+        //     for(int j=1;j<=i;j++){
+        //         System.out.print(i+" ");
+        //     }
+        //     System.out.println();
+        // }
+
+        // for(int i=0;i<=n;i++){
+        //     for(int j=1;j<=n-i;j++){
+        //         System.out.print(" ");
+        //     }
+        //     for(int j=i;j>=1;j--){
+        //         System.out.print(j);
+        //     }
+        //     for(int j=2;j<=i;j++){
+        //         System.out.print(j);
+        //     }
+        //     System.out.println();
+
+        // }
+
+        //Diamond Pattern : write pattern for first half and for the second half just reverse it
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=2*(i-1)+1;j++){
+                System.out.print("*");
+            }
+            
             System.out.println();
+
         }
 
         for(int i=n;i>=1;i--){
-            for(int j=1;j<=i;j++){
-                System.out.print("* ");
+            for(int j=1;j<=n-i;j++){
+                System.out.print(" ");
             }
-            for(int j=1;j<=2*(n-i);j++){
-                System.out.print("  ");
+            for(int j=1;j<=2*(i-1)+1;j++){
+                System.out.print("*");
             }
-            for(int j=1;j<=i;j++){
-                System.out.print("* ");
-            }
+            
             System.out.println();
+
         }
 
     }
---
+++ Functions.java
@@ -0,0 +1,30 @@
+import java.util.*;
+public class Functions {
+    public static void enterNumbers(){
+        Scanner scanner = new Scanner(System.in);
+        int post=0;
+        int negt=0;
+        while (true) {
+            System.out.println("Choose 1 to Enter an number");
+            System.out.println("Choose 2 to exit");
+            int choice=scanner.nextInt();
+            if (choice==1) {
+                int number=scanner.nextInt();
+                if(number>0){
+                    post++;
+                }
+                else if(number<0){
+                    negt++;
+                }
+            }
+            else{
+                System.out.println("Positive numbers entered: "+post);
+                System.out.println("Negative numbers entered: "+negt);
+                return;
+            }
+        }
+    }
+    public static void main(String[] args) {
+        enterNumbers();
+    }
+}
